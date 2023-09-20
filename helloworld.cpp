cfd669e440c2c6e6c672987f038330db9b26377a
Ömer Bilici <102596640+omerblc@users.noreply.github.com>
2023-09-20 16:11:00 +0300

    helloworld

---
+++ helloworld.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+/* #include is a way of including a standard or user-defined file in the program and 
+is mostly written at the beginning of any C/C++ program. */
+
+// The iostream file contains code that allows a C++ program to display output on the screen and read input from the keyboard.
+
+int main(){
+	std::cout << "Hello World!" << std::endl;
+	return 0;
+}
